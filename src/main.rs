8fa4a801d376caba638c6148609ee264ecac416e
purfakt <sdethuin@gmail.com>
2023-05-07 21:28:35 +0200

    Chapter 7.3.10

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
-use sqlx::postgres::PgPoolOptions;
-use std::net::TcpListener;
 use zero2prod::{
     configuration::get_configuration,
-    startup::run,
+    startup::Application,
     telemetry::{get_subscriber, init_subscriber},
 };
 
@@ -13,15 +11,10 @@ async fn main() -> Result<(), std::io::Error> {
 
     let configuration = get_configuration().expect("Failed to read configuration.");
 
-    let pg_pool = PgPoolOptions::new()
-        .acquire_timeout(std::time::Duration::from_secs(2))
-        .connect_lazy_with(configuration.database.with_db());
+    let _ = Application::build(configuration)
+        .await?
+        .run_until_stopped()
+        .await?;
 
-    let address = format!(
-        "{}:{}",
-        configuration.application.host, configuration.application.port
-    );
-    let listener = TcpListener::bind(address)?;
-
-    run(listener, pg_pool)?.await
+    Ok(())
 }
